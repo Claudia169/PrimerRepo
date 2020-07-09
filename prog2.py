53ee75833ba7226f916bd795cfed12b938eee09e
Claudia Morón Fernández <claudya169@gmail.com>
2020-07-09 10:12:53 -0500

    Programa 2 finalizado

---
+++ prog2.py
@@ -0,0 +1,11 @@
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa otro numero: "))
+suma = 0
+
+num1 = num2 +1
+
+for num in rango (num1, num2):
+    suma += num
+print (suma)
+
+
